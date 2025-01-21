5ffc0df9165487bca2f9235a2ad20b926c2c6c8d
John Wu <azxenon3@gmail.com>
2025-01-21 12:30:12 +0800

    keymap setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local remap = LazyVim.safe_keymap_set
+remap("i", "ii", "<Esc>")
+remap("n", "a", "A")
+remap("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
